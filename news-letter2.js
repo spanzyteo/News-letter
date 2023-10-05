cf2687c1cb32d46bfe281df9273bea78914f6b7e
spanzy teo <spanzytheo@gmail.com>
2023-10-05 04:04:14 +0100

    changes mads to news-letter
    description

--- news-letter2.js
+++ news-letter2.js
@@ -1,16 +1,13 @@
 const displayEmail = JSON.parse(localStorage.getItem('emailInput'))
 const display = document.querySelector('.js-mail')
-document.querySelector('.js-message')
-    .addEventListener('click', ()=> {
-        window.location.href="news-letter.html"
-    })
-
-document.body.addEventListener('keydown', (event)=> {
-    if (event.key === 'Enter'){
-        window.location.href="news-letter.html"
-    }
+document.querySelector('.js-message').addEventListener('click', () => {
+  window.location.href = 'index.html'
 })
 
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    window.location.href = 'index.html'
+  }
+})
 
 display.innerHTML = displayEmail
-
